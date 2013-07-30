c6a509586fc0c7cf5309ce55ec66356fbcb95121
John W. Clark <john.wilbur.clark@gmail.com>
2013-07-30 14:51:21 -0400

    add test driver

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () 
+{
+    int i;
+	printf("Hello, world!\n");
+	
+	for ( i = 0 ; i < 10 ; ++i )
+	{
+		printf("%d ", 10 - i );
+	}
+	printf("\n");
+	
+	return 0;
+}
